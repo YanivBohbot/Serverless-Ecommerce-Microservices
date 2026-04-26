100b91f3406c25538d36d0d4a5cd5da919678b87
Yaniv Bohbot <yanivbohbot5@gmail.com>
2026-04-26 11:37:19 +0300

    fixed

--- order-system/services/notification-service/src/providers/email.provider.ts
+++ order-system/services/notification-service/src/providers/email.provider.ts
@@ -2,7 +2,7 @@ export class EmailProvider {
   async sendOrderConfirmation(orderId: number, status: string) {
     console.log(`\n--- 📧 NEW NOTIFICATION ---`);
     console.log(`To: customer@example.com`);
-    console.log(`Message: Order #${orderId} has been successfully ${status}!`);
+    console.log(`Message kik: Order #${orderId} has been successfully ${status}!`);
     console.log(`  Timestamp : ${new Date().toISOString()}`);
     console.log(`---------------------------\n`);
   }
